2d361d34f1ffa13a3f28f858add619a4de3c25c2
Brendan <brendan.harmon@gmail.com>
2023-11-19 12:15:32 -0600

    updated map

--- public/js/map.js
+++ public/js/map.js
@@ -7,6 +7,7 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 }).addTo(mymap);
+mymap.invalidateSize();
 
 // create custom markers
 var markerIcon = new L.Icon({
